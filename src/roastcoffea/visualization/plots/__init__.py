901633e4243c3be44f006c534c9ce6d95a317170
Mohamed Aly <89147478+MoAly98@users.noreply.github.com>
2025-12-10 13:13:28 +0000

    expose more plotting

--- src/roastcoffea/visualization/plots/__init__.py
+++ src/roastcoffea/visualization/plots/__init__.py
@@ -10,6 +10,11 @@ from roastcoffea.visualization.plots.cpu import (
     plot_occupancy_timeline,
 )
 from roastcoffea.visualization.plots.memory import plot_memory_utilization_timeline
+from roastcoffea.visualization.plots.per_task import (
+    plot_per_task_bytes_read,
+    plot_per_task_cpu_io,
+    plot_per_task_overhead,
+)
 from roastcoffea.visualization.plots.scaling import (
     plot_efficiency_summary,
     plot_resource_utilization,
@@ -26,6 +31,9 @@ __all__ = [
     "plot_executing_tasks_timeline",
     "plot_memory_utilization_timeline",
     "plot_occupancy_timeline",
+    "plot_per_task_bytes_read",
+    "plot_per_task_cpu_io",
+    "plot_per_task_overhead",
     "plot_resource_utilization",
     "plot_throughput_timeline",
     "plot_total_active_tasks_timeline",
